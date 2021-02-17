342a4a46ba05105c605fca901fbb88844333fea4
Anders Åslund <anders.aslund@teknoir.ai>
2021-02-17 21:14:52 +0100

    Patch 001

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Hello Teknoir!"
-
 tmp_dir=$(mktemp -d -t sustayn-patch-001XXX)
-echo $tmp_dir
 pushd $tmp_dir
-
 curl -Lo patch.zip https://github.com/teknoir/sustayn_os_patch_001/releases/download/patch001/patch.zip
 echo "Please be ready to enter the zip file password:"
 unzip patch.zip
