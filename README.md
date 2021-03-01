884e84988dd208f631503ee12f9320fdb820cff9
Anders Åslund <anders.aslund@teknoir.ai>
2021-03-01 08:58:59 +0100

    Minipatch revisited

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ sudo reboot
 
 # Mini Patch 001.1
 
-We missed some settings for the ethernet interface, the patch above is also patched to include this patch.
+We missed some settings for the ethernet interface and some nvidia drivers, the patch above is also patched to include this patch.
 If the patch above is run from now on there should be no reason to use this mini patch 
 
 > This patch fix the eth0 interface settings and enable it to connect to the Axis camera.
@@ -22,4 +22,7 @@ In the terminal(SSH) enter the following command to apply the patch:
 bash <(curl -Ls https://raw.githubusercontent.com/teknoir/sustayn_os_patch_001/main/mini_patch_001.1.sh)
 ```
 
-You do not have to reboot the device.
+And reboot:
+```
+sudo reboot
+```
--- mini_patch_001.1.sh
+++ mini_patch_001.1.sh
@@ -1,12 +1,37 @@
 #!/usr/bin/env bash
 set -xe
-sudo tee <<EOL "/etc/systemd/network/20-ethernet.network"
+
+SUDO=''
+if (( $EUID != 0 )); then
+    echo "Please be ready to enter the device´s sudo password:"
+    SUDO='sudo'
+fi
+
+$SUDO tee <<EOL "/etc/systemd/network/20-ethernet.network"
 [Match]
 Name=eth0
 
 [Network]
 Address=192.168.0.70/24
 EOL
-sudo ifconfig eth0 down
-sudo ifconfig eth0 up
-sudo systemctl restart systemd-networkd
+$SUDO ifconfig eth0 down
+$SUDO ifconfig eth0 up
+$SUDO systemctl restart systemd-networkd
+
+# Install cuda drivers
+$SUDO apt-get install -y cuda-compiler-10-2 cuda-cudart-10-2 cuda-libraries-10-2 cuda-toolkit-10-2 cuda-tools-10-2 nvidia-container-csv-cuda
+
+# Make sure nvidia runtime is set by default
+$SUDO apt-get install -y nvidia-container-runtime nvidia-docker2 jq
+
+$SUDO tee <<EOL "/etc/docker/daemon.json"
+{
+  "default-runtime": "nvidia",
+  "runtimes": {
+    "nvidia": {
+      "path": "nvidia-container-runtime",
+      "runtimeArgs": []
+    }
+  }
+}
+EOL
