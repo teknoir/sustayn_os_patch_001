01c1f08fdb7bf7424cdff4a249144b4a078adc58
Anders Åslund <anders.aslund@teknoir.ai>
2021-03-12 22:43:03 +0100

    Make space patch

--- make_space_patch_001.sh
+++ make_space_patch_001.sh
@@ -7,8 +7,7 @@ if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 
-
-echo "Space on device before patch!"
+echo "Space on device before freeing up space!"
 $SUDO df -H /dev/mmcblk0p1
 
 # Completely remove docker
@@ -16,6 +15,19 @@ $SUDO apt-get purge -y docker-ce docker-ce-cli || true
 $SUDO rm -rf /var/lib/docker || true
 $SUDO apt autoremove --purge -y || true
 $SUDO apt clean || true
+
+# Remove unused images from containerd
+sudo k3s crictl rmi --prune
+
+echo "Space on device after freeing up space!"
+$SUDO df -H /dev/mmcblk0p1
+
+echo "Install nvidia bloatware"
+# Install cuda drivers & nvidia-runtime
+$SUDO apt-get install -y \
+  cuda-compiler-10-2 cuda-cudart-10-2 cuda-libraries-10-2 cuda-toolkit-10-2 cuda-tools-10-2 nvidia-container-csv-cuda \
+  nvidia-container-runtime jq libsystemd-dev p7zip minicom Jetson.GPIO python3-serial python3-pip isc-dhcp-client \
+  networkd-dispatcher
 
 echo "Space on device after patch!"
 $SUDO df -H /dev/mmcblk0p1
